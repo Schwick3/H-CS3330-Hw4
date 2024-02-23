064e72ed38a6c4ccdc207988fa8e6bfd8482c773
Henry Clark <hry1776@gmail.com>
2024-02-23 16:54:04 -0600

    Revert "Implemented is"
    This reverts commit 9a59d518c06349c43a7daf575f9fde22de78e5e6.

--- src/edu/mu/vehicles/VehicleManager.java
+++ src/edu/mu/vehicles/VehicleManager.java
@@ -182,13 +182,6 @@ public class VehicleManager {
 			return false;
 		}
 	}
-	private boolean isVehicleType(Vehicle v, Class clazz ) {
-		
-		if(v.getClass().equals(clazz)) {
-			return true;
-		}
-		else return false;
-	}
 	
 	
 	
