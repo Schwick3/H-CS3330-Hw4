c66c96a78d5c42f474f4cba792fd69ad21dcc6ef
Kyle Blank <kcgblank@gmail.com>
2024-02-23 19:14:13 -0600

    added calculateFuelEfficiency for MotorBike subclass

--- src/edu/mu/vehicles/MotorBike.java
+++ src/edu/mu/vehicles/MotorBike.java
@@ -4,4 +4,9 @@ public class MotorBike extends Vehicle {
 	public MotorBike(String type, String brand, String make, long modelYear, double price, VehicleColor color, FuelType fuelType, double mileage, double mass, int cylinders, double gasTankCapacity, StartMechanism startType ) {
 		super(type, brand, make, modelYear, price, color, fuelType, mileage, mass, cylinders, gasTankCapacity, startType);
 }
+	@Override
+	public double calculateFuelEfficiency(double distance, double fuelPrice) {
+		double fuelEfficiency = cylinders*gasTankCapacity*fuelPrice/distance*0.001;
+		return fuelEfficiency;
+	}
 }
