9348442980e5f8974c0c3bbab2da031da9ae4527
Schwick3 <sky321949@gmail.com>
2024-02-24 22:19:56 -0600

    Added small fixes and tests to main
    Fixed spacing within the toString method

--- src/edu/mu/Main.java
+++ src/edu/mu/Main.java
@@ -5,6 +5,19 @@ import edu.mu.vehicles.*;
 public class Main {
 
 	public static void main(String[] args) {
+		VehicleManager vehiclemanager = new VehicleManager();
+		
+		System.out.println("VEHICLE INFORMATION");
+		System.out.println("*******************");
+
+		vehiclemanager.displayAllVehicleInformation();
+		System.out.println("CAR INFORMATION");
+		System.out.println("***************");
+		vehiclemanager.displayAllCarInformation();
+		System.out.println("MOTORBIKE INFORMATION");
+		System.out.println("*********************");
+		vehiclemanager.displayAllMotorBikeInformation();
+		
 	}
 
 }
